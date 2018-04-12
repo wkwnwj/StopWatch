dc946533dd23c47fa038b77a5baf12a2f74a71ce
csd5766 <csd5766@naver.com>
2018-04-12 11:26:19 +0900

    Chan

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     
     var count = 0
     var myTimer = Timer()
+    //시간 관련된걸 작성할때 Timer()메소드를 사용해줘야 됨
+    
     
     func changeButoonState(start: Bool, stop: Bool, reset: Bool ) {
         Start.isEnabled = start
@@ -24,6 +26,8 @@ class ViewController: UIViewController {
         Reset.isEnabled = reset
         //작동하도록 하는게 isEnabled
         //작동안하도록 하는게 disEnabled
+        //이 (func)함수를 사용하려면 위에 Outlet 변수를 선언 해줘야 한다.
+        //이 함수를 사용한 이유는 밑에 보면 버튼을 클릭 하였을때 더이상 눌러지지 않도록 하기 위해 이 함수를 사용
     }
     
     override func viewDidLoad() {
@@ -49,12 +53,14 @@ class ViewController: UIViewController {
     
     @IBAction func Stop(_ sender: Any) {
         myTimer.invalidate()
+        //멈추는 메소드
         changeButoonState(start:true, stop: false, reset: true)
     }
     
     @IBAction func Reset(_ sender: Any){
         a.text = ("00:00:00")
         count = 0
+        //reset은 count의 값이 남아 있으면 안되기 때문에 0
         changeButoonState(start: true, stop: true, reset: false)
     }
     
